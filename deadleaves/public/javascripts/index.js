e4841f92f90f1bba92c3ef1e63c4e7dae37b706e
dcjohnston <djohnston2014@gmail.com>
2016-03-06 18:34:18 -0500

    add image upload preview

--- deadleaves/public/javascripts/index.js
+++ deadleaves/public/javascripts/index.js
@@ -3,5 +3,19 @@ $(function(){
     height: '300px',
     width: '300px'
   });
+
+  $('#image').change(function(ev){
+    var target = this.files[0];
+    var thumbnail = $('#selected-image')[0];
+
+    var reader = new FileReader();
+    reader.onload = function() {
+      console.log(reader);
+      thumbnail.src = reader.result;
+    };
+    console.log(target);
+    reader.readAsDataURL(target);
+
+  });
 });
 // http://stackoverflow.com/questions/14069421/show-an-image-preview-before-upload
