538d208ab615dadb1a12eb73f388f74044a6dcb9
dcjohnston <djohnston2014@gmail.com>
2016-03-06 23:16:13 -0500

    add multer

--- deadleaves/routes/api.js
+++ deadleaves/routes/api.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var multer = require('multer');
 var router = express.Router();
-
-router.post('/task', function (req, res, next){
+var upload = multer({ dest: 'uploads/'})
+router.post('/task', upload.single('image'), function (req, res, next){
   var POST = req.body;
-  console.log(POST);
+  var image = req.file;
+  console.log(image);
 });
 
 module.exports = router;
