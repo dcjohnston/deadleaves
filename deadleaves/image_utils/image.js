516f51bbc65576a9cc80d266c86c4f5b053670e2
Derek Johnston <derek@epidemico.com>
2016-03-14 20:49:54 -0400

    update git ig

---
+++ deadleaves/image_utils/image.js
@@ -0,0 +1,18 @@
+function InversePowerCDF(alpha, xmin) {
+  xmin = xmin || 10; //lol
+  if (typeof alpha === "function") {
+    alpha = alpha();
+  }
+  return function(p) {
+    return Math.pow(alpha*xmin*Math.pow(p, -1/(alpha + 1))
+  };
+}
+
+module.exports = function (emojiArray) {
+  return {
+    scales: emojiArray.map(InversePowerCDF(Math.random)),
+    locations: emojiArray.map(function (e) {
+      return [Math.random(), Math.random()];
+    })
+  };
+};
--- deadleaves/routes/api.js
+++ deadleaves/routes/api.js
@@ -4,7 +4,7 @@ var emojione = require('emojione')
 var fs = require('fs');
 var path = require('path');
 var router = express.Router();
-
+var imageUtils = require('../image_utils/image')
 var upload = multer({
   dest: 'uploads/'
 });
@@ -16,9 +16,11 @@ router.post('/task', upload.single('image'), function (req, res, next){
       image = req.file,
       emoji = emojione.unicodeToImage(post.emoji),
       emojiId = emoji.match(/.*\/assets\/svg\/([a-z, 0-9]*\.svg).*/)[1],
-      targetSvg = path.join(__dirname, '../node_modules/emojione/assets/svg', emojiId);
-      targetImage = path.join(__dirname, '../uploads', image.filename);
+      targetSvg = path.join(__dirname, '../node_modules/emojione/assets/svg', emojiId),
+      targetImage = path.join(__dirname, '../uploads', image.filename)
+      processorAPI = imageUtils(post.emoji);
 
+  console.log(post, image);
 });
 
 module.exports = router;
