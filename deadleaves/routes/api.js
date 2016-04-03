8b016142508a6eae9fa4691aaf37344cb8b3cd90
Derek Johnston <derek@epidemico.com>
2016-04-03 18:58:03 -0400

    write cli interface

--- deadleaves/routes/api.js
+++ deadleaves/routes/api.js
@@ -5,6 +5,7 @@ var express = require('express'),
   path = require('path'),
   router = express.Router(),
   Parametrize = require('../image_utils/parametrize'),
+  PythonShell = require('python-shell'),
   ScaleImage = require('../image_utils/scale_svg');
   // upload = multer({
   //   dest: 'uploads/'
@@ -18,33 +19,31 @@ router.post('/task', function(req, res, next) {
     emojishorts = emojione.toShort(post.emoji).split('::'),
     alpha = post.intensity,
     emojiIds = emojis.match(/(?:\/([a-z, 0-9]*\.svg))/g);
-    // targetimage = path.join(__dirname, '../uploads', image.filename),
-    // xymap = Parametrize(emojishorts.length, alpha);
 
-  // //transform emoji index into url to upstream svg
-  // xymap = xymap.map(function(parameters) {
-  //   var eName = emojiIds[parameters[3]];
-  //   parameters[3] = path.join(__dirname, '../node_modules/emojione/assets/svg', eName);
-  //   return parameters;
-  // })
-  //
-  // //.map(function (parameters) {
-  // //   ScaleImage(parameters[3], parameters[2], function (svg) {
-  // //     console.log(svg);
-  // //   });
-  // // });
-  //
-  // var result = fs.readFileSync(targetimage);
-  var base64data = new Buffer('foobar').toString('base64');
-  //encoding binary -> utf-8 string
-  res
-    .header({
-      'Content-Type': 'application/json'
-    })
-    .send({
-      // image: base64data,
-      name: 'DISABLED'
-    });
+  var cliArguments = {
+    mode: '',
+    scriptPath: path.join(__dirname, '../python_scripts',
+
+  }
+
+  PythonShell.run('emojify.py', cliArguments, function (err, results) {
+    if (err) {
+      throw err;
+    } else {
+      //encoding binary -> utf-8 string
+      var processedImage = fs.readFile(results, function (err, data) {
+        var base64data = new Buffer(data).toString('base64');
+        res.header({
+            'Content-Type': 'application/json'
+          })
+          .send({
+            image: base64data,
+            name: 'emoji_pile.png'
+          });
+      });
+
+    }
+  });
 
 });
 
