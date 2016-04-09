447c69e8efef069d47766c269a3a6210cf3380b3
Derek Johnston <derek@epidemico.com>
2016-04-09 15:51:37 -0400

    add gallery

--- deadleaves/routes/index.js
+++ deadleaves/routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -12,6 +14,16 @@ router.get('/about', function (req, res, next) {
   res.render('about', {
     title: 'Emoji Piles'
   })
+});
+
+router.get('/gallery', function (req, res, next) {
+
+  fs.readdir(path.join(__dirname, '../public/images/gallery'), function (err, files) {
+    res.render('gallery', {
+      title: 'Emoji Piles',
+      images: files
+    });
+  });
 });
 
 module.exports = router;
